53bef209373b4e68c88464f08041782fb8a565cd
yjm <yejiangmingpku@gmail.com>
2025-06-05 08:21:25 -0700

    add ugetpid

--- kernel/proc.c
+++ kernel/proc.c
@@ -196,6 +196,19 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map USYSCALL page 
+  char* pa = kalloc();
+  if(mappages(pagetable, USYSCALL, PGSIZE, (uint64)pa, PTE_R|PTE_U) < 0){
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
+  struct usyscall *usyscall = (struct usyscall *)pa;
+  usyscall->pid = p->pid;
+
+
   return pagetable;
 }
 
@@ -206,6 +219,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 1);
   uvmfree(pagetable, sz);
 }
 
