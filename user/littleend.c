aec49f68a83846ec897a2031b3d84adeab90edd3
yjm <yejiangmingpku@gmail.com>
2025-07-03 09:02:06 -0700

    riscv-assembly

---
+++ user/littleend.c
@@ -0,0 +1,16 @@
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+
+int
+main(int argc, char *argv[])
+{
+    // unsigned int i = 0x00646c72;
+    // printf("H%x Wo%s\n", 57616, &i);
+
+    printf("x=%d y=%d\n", 3);
+    // 
+    exit(0);
+}
